570a57667b2b7a10fd281f266cef4403d31a9c57
DensyVilomar <133794018+DensyVilomar@users.noreply.github.com>
2023-06-09 21:28:15 -0400

    Update P-Ejercicio16.py

--- P-Ejercicio16.py
+++ P-Ejercicio16.py
@@ -2,7 +2,7 @@ def next_age():
     year = int(input("what year is this?\n"))
     for i in range(3):
         name = input("What's your name: ")
-        your_year = int(input("Where were you born: "))
+        your_year = int(input("When were you born: "))
         next_age = year - your_year
         print(f"{name} will be {next_age} years old this year.\n")
         
