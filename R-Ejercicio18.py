d14d3dd09138dcc9075c8eac3f004646a9733b92
DensyVilomar <densy.vilomar09@gmail.com>
2023-06-12 06:24:28 -0400

    Interesting

--- R-Ejercicio18.py
+++ R-Ejercicio18.py
@@ -2,22 +2,16 @@ def search_names(list_of_names,letter):
     
     cont = 0
     
-    try:
-        
-        for names in list_of_names:
-            if names.startswith(letter.upper()):
-                cont += 1
-                print(names) ### Innecesario ###
+    for names in list_of_names:
+        if names.startswith(letter.upper()):
+            cont += 1
+            print(names) ### Innecesario ###
                 
-        if len(letter) > 1:
-            print("Introduzca una sola letra.")
-        else:  
-            print(f"Hay {cont} nombres que inician con la letra {letter.upper()}.")
-        
-    except AttributeError:
-        print("Introduzca una letra del abecedario.")
-        
-        
+    if len(letter) > 1:
+        print("Introduzca una sola letra.")
+    else:  
+        print(f"Hay {cont} nombres que inician con la letra {letter.upper()}.")
+                
 lis = ["Densy","Jose","Daniela","Darlin","Manuel","Gilbert","Samara","Jesus","Marcos","Samuel"]
 
 search_names(lis,"d")
