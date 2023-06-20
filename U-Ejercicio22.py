410cf369da67824ab54854348d6179303c2e9e31
DensyVilomar <densy.vilomar09@gmail.com>
2023-06-20 16:06:50 -0400

    Principio.

--- U-Ejercicio22.py
+++ U-Ejercicio22.py
@@ -1,6 +1,6 @@
 def interes_anual(amount,interest,years):
     
-    value = amount * (1 + interest/100) ** years
-    return value
+    final_value = amount * (1 + interest/100) ** years
+    return final_value
 
 print(interes_anual(10000,4.5,20))
